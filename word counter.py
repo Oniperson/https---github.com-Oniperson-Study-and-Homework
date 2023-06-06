6d9e9cdeb3bca6e3b3ec45eb7f64e1daf3eee9d1
BRANDON_INCE <BRANDONCINCE@GMAIL.COM>
2023-06-06 12:11:31 +0900

    first commit

---
+++ word counter.py
@@ -0,0 +1,8 @@
+list = "Help! Not just anybody Help! You know I need someone Help!"
+list = list.split()
+count = 0
+help = "Help!"
+for character in list:
+    if character in help:
+        count +=1
+print (count)
