916d3da5f22459fdb13531f008d3b07d0d7e2bd8
BRANDON_INCE <BRANDONCINCE@GMAIL.COM>
2023-06-27 12:49:43 +0900

    Letter organizer temp solution

--- letter organizer.py
+++ letter organizer.py
@@ -1,14 +1,14 @@
-s = "abcdpoopsdfasjfkjlhmvfabcdefgajlajhaz"
-mystery = ""
-mysterytwo = ""
-
+s = "abcdbwxyz"
+longest_string = ""
+current_string = ""
+s += " "  
 for i in range(1,len(s)):
     if s[i-1] <= s[i]:
-        mysterytwo += s[i-1]
-    elif len(mystery) <= len(mysterytwo):
-            mysterytwo += s[i-1]
-            mystery = mysterytwo
-            mysterytwo = ""
+        current_string += s[i-1]
+    elif len(longest_string) <= len(current_string):
+            current_string += s[i-1]
+            longest_string = current_string
+            current_string = ""
     else:
-         mysterytwo = ""
-print (mystery)
+         current_string = ""
+print (longest_string)
